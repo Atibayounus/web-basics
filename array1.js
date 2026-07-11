5b40fe32f8eab288265de7870e2f214d7e054ea7
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-07-11 20:03:15 +0500

    practicing array problems

---
+++ array1.js
@@ -0,0 +1,10 @@
+//arrays
+//let marks=[10,20,30,40];
+//console.log(marks);
+//console.log(marks.length);
+//Access Elements
+
+const colors = ["Red", "Green", "Blue", "Yellow"];
+
+console.log(colors[0]);
+console.log(colors[colors.length - 1]);
---
+++ array2.js
@@ -0,0 +1,10 @@
+//changing element of array
+const Animals=["cat","dog","donkey","lizard"];
+Animals[1]="cow";
+console.log(Animals);
+
+//length of array
+
+const cities = ["Lahore", "Karachi", "Islamabad"];
+
+console.log(cities.length);
