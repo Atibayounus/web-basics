aa3b5f368bcd8f594fba5dca622b7495451f79d6
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-07-12 00:52:24 +0500

    variables in js

---
+++ variables.js
@@ -0,0 +1,33 @@
+// Variables in JavaScript
+
+// 1. let 
+let name = "Atibaaaa";
+console.log("Name:", name);
+
+name = "Peter Parker";
+console.log("Updated Name:", name);
+
+// 2. const 
+const country = "Pakk";
+console.log("Country:", country);
+
+
+// Different data types
+let firstName = "Bruce";      // String
+let heroAge = 40;             // Number
+let isHero = true;            // Boolean
+let salary = 50000.75;        // Decimal Number
+let city = null;              // Null
+let state;                    // Undefined
+
+console.log(firstName);
+console.log(heroAge);
+console.log(isHero);
+console.log(salary);
+console.log(city);
+console.log(state);
+
+// Using variables together
+let fullMessage = firstName + " is " + heroAge + " years old.";
+console.log(fullMessage);
+
