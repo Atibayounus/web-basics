a849584956fb022b5e7428de79f1687c023984aa
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-07-22 18:48:42 +0500

    Update array2.js

--- array2.js
+++ array2.js
@@ -8,3 +8,13 @@ console.log(Animals);
 const cities = ["Lahore", "Karachi", "Islamabad"];
 
 console.log(cities.length);
+
+// Given an array of numbers, e.g. [4, 8, 15, 16, 23, 42], write code that returns a NEW array containing only the even numbers, doubled.
+const numbers = [4, 8, 15, 16, 23, 42];
+const result = [];
+for(let i=0 ; i<numbers.length ; i++){
+if(numbers[i]%2 === 0){
+    result.push(numbers[i]*2);
+}
+}
+console.log(result);
